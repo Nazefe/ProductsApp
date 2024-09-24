2d84ef1384dede27c9a380a53d078a87a18b084a
bredihin.na <nazarbredixin@gmail.com>
2024-09-24 16:15:24 +0300

    Goppo

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        DbConnectionImpl connection = new DbConnectionImpl();
+
+            connection.showData(connection.connect());
+    }
+}
---
+++ src/main/java/DbConnection.java
@@ -0,0 +1,31 @@
+import org.postgresql.Driver;
+
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public interface DbConnection {
+
+    default Connection connect(){
+       try{
+           Class.forName("org.postgresql.Driver");
+           Connection connection = DriverManager.getConnection(
+                   "jdbc:postgresql://localhost:5432/products db", "postgres", "admin"
+           );
+           System.out.println("Connection succeed!");
+           return connection;
+       }
+       catch (ClassNotFoundException | SQLException e) {
+           System.out.println("Connection failed! Please try again!");
+           return null;
+       }
+    }
+    void insert();
+
+    void update();
+
+    void select();
+
+    void delete();
+}
